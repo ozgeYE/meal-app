e3145eb08f88d408519f3fe6e7f6991e0e794a26
ozgeYE <ozge.iney@gmail.com.tr>
2019-12-19 21:03:33 +0300

    feat: add dummy data, interfaces, constants and conf files

---
+++ data/dummy-data.ts
@@ -0,0 +1,13 @@
+import {Category} from "../interfaces";
+
+const CATEGORIES: Category[] = [
+  {id: 1, name: 'Turkish', color: '#a839fc'},
+  {id: 2, name: 'Italian', color: '#45fc39'},
+  {id: 3, name: 'French', color: '#39b4fc'},
+  {id: 4, name: 'Hamburgers', color: '#fc3942'},
+  {id: 5, name: 'Asian', color: '#f0fc39'},
+  {id: 6, name: 'German', color: '#39fcbd'},
+  {id: 7, name: 'Exotic', color: '#fc39a5'},
+];
+
+export default CATEGORIES;
